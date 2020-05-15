789df094c4c72c2a16ba24ab56de5b677a3a124f
Piepognwong <j.tonneyck@gmail.com>
2020-05-15 16:00:25 +0200

    with classes

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-// function isCollide($element1, $element2) {
-//     var a = {
-//         y: 100 - $element1.offsetTop - $element1.height, 
-//         x: $element1.offsetLeft,
-//         height: $element1.height,
-//         width: $element1.width
-//     }
-//     var b = {
-//         y: 100 - $element2.offsetTop - $element2.height, 
-//         x: $element2.offsetLeft,
-//         height: $element2.height,
-//         width: $element2.width
-//     }
-
-//     return !(
-//         ((a.y + a.height) < (b.y)) ||
-//         (a.y > (b.y + b.height)) ||
-//         ((a.x + a.width) < b.x) ||
-//         (a.x > (b.x + b.width))
-//     );
-// }
-
 let $box1 = document.querySelector(".box-1");
 let $box2= document.querySelector(".box-2");
 
@@ -49,47 +27,15 @@ function collisionDetectIon($dom1,$dom2){
         console.log("No collision!");
         return false
     }
-    // Method 2. Only works with squares and is more verbose.
-    // if( (sq2.y >= sq1.y && sq2.y <= sq1.y + sq1.height) && (sq2.x >= sq1.x && sq2.x <= sq1.x + sq1.width) ||
-    //     (sq2.y >= sq1.y && sq2.y <= sq1.y + sq1.height) && (sq2.x + sq2.width >= sq1.x && sq2.x + sq2.width <= sq1.x + sq1.width) ||
-    //     (sq2.y + sq2.height >= sq1.y && sq2.y + sq2.height <= sq1.y + sq1.height) && (sq2.x >= sq1.x && sq2.x <= sq1.x + sq1.width) ||
-    //     (sq2.y + sq2.height >= sq1.y && sq2.y + sq2.height <= sq1.y + sq1.height) && (sq2.x + sq2.width >= sq1.x) && (sq2.x + sq2.width <= sq1.x + sq1.width)
-    // ) {
-    //     console.log("Collision!");
-    //     return true;
-    // } else {
-    //     console.log("NO collision!");
-    //     return false;
-    // }
 }
 
 
 let $box3 = document.querySelector(".box-3");
-let $grandma = document.querySelector("#grandma");
 
 // let $box4= document.querySelector(".box-4");
 // collisionDetectIon($box1, $box2);
 // collisionDetectIon($box3, $box4);
-let $car = document.querySelector("#car");
-document.addEventListener("keydown", function(event){
-    switch (event.key.toLowerCase()) {
-        case "d":
-            $car.style.left = `${$car.offsetLeft + 10}px`;
-            break;
-        case "a":
-            $car.style.left = `${$car.offsetLeft - 10}px`;
-            break;
-        case "w":
-            $car.style.top = `${$car.offsetTop - 10}px`;
-            break;
-        case "s":
-            $car.style.top = `${$car.offsetTop + 10}px`;
-            break;
-        default:
-            break;
-    }
-    if(collisionDetectIon($grandma, $car)) alert("You hit grandma. BOooooe!");
-})
+
 
 // setInterval(()=> {
 //     $grandma.style.left = `${$grandma.offsetLeft + 10}px`
@@ -112,3 +58,63 @@ let $body = document.querySelector("body");
 //         $police.style.top = `${$police.offsetTop + 10}px`;
 //     })
 // },500)
+
+class Grandma {
+    render(){
+        let $grandma = document.querySelector("#grandma");
+        $grandma.style.left = `${$grandma.offsetLeft + 10}px`;
+    }
+}
+
+class PoliceOfficer {
+    constructor(){
+        let $police = document.createElement("img");
+        $police.src = "./police.jpg";
+        $police.setAttribute("class", "police");
+        $body.appendChild($police);
+        $police.style.left = `${Math.random() * 100}%`;
+    }
+
+    render(){
+        if(collisionDetectIon($police, $car)) alert("Busted!");
+        $police.style.top = `${$police.offsetTop + 10}px`;
+    }
+}
+
+class Car {
+    constructor(){
+        let $car = document.querySelector("#car");
+        document.addEventListener("keydown", function(event){
+            switch (event.key.toLowerCase()) {
+                case "d":
+                    $car.style.left = `${$car.offsetLeft + 10}px`;
+                    break;
+                case "a":
+                    $car.style.left = `${$car.offsetLeft - 10}px`;
+                    break;
+                case "w":
+                    $car.style.top = `${$car.offsetTop - 10}px`;
+                    break;
+                case "s":
+                    $car.style.top = `${$car.offsetTop + 10}px`;
+                    break;
+                default:
+                    break;
+            }
+            if(collisionDetectIon($grandma, $car)) alert("You hit grandma. BOooooe!");
+        })
+    }
+}
+
+class Game {
+    car = new Car();
+    grandma = new Grandma();
+    officers = [new Officer()];
+
+    render(){
+        setInterval(()=> {
+            this.grandma.render();
+            this.officers.render();
+        }, 2000)
+    }
+}
